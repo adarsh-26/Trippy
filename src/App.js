8888b2b16ca4ea1ee2e212cfeb57c7b37fee4174
adarsh-26 <96662112+adarsh-26@users.noreply.github.com>
2024-03-08 23:37:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Service from './routes/Service';
+import Contact from './routes/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}/>
+        <Route path="/about" element={<About></About>}/>
+        <Route path="/service" element={<Service></Service>}/>
+        <Route path="/contact" element={<Contact></Contact>}/>
+
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
